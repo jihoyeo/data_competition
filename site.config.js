b4a7f8a2bb32cb3153ce7139ca3c1338faf408ba
Jiho Yeo <33746520+jihoyeo@users.noreply.github.com>
2023-10-31 05:41:24 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "HNU",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/mark22.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "",
     bio: "Welcome!.",
     email: "jihoyeo@hnu.kr",
