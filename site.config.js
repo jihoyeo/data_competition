21c381119868a74671ad6287b3b620b610653d4d
Jiho Yeo <33746520+jihoyeo@users.noreply.github.com>
2023-10-31 05:22:10 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -12,19 +12,19 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `hnu_data_competition`,
+      href: "https://github.com/jihoyeo/data_competition/",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
+    title: "HNU_data_competition",
     description: "welcome to morethan-log!",
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "https://data-competition.vercel.app",
+  since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
